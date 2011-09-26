d78348df044cfe049b6b849b6855476fc3490064
Luis Dario Simonassi <ldsimonassi@gmail.com>
2011-09-25 23:10:29 -0300

    failing test

---
+++ src/test/groovy/AbstractTest.groovy
@@ -0,0 +1,7 @@
+import junit.framework.Assert;
+
+
+class AbstractTest extends Assert{
+
+}
+
---
+++ src/test/groovy/CoreGameTest.groovy
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class CoreGameTest extends AbstractTest {
+
+	@Test
+	public failForSure() {
+		assert false
+	}
+
+}
